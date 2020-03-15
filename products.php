7d2457a1c2cd58a5ab0198ddc49462d0ad26334a
Taoufiq Benallah <taoufiq.benallah@gmail.com>
2020-03-15 19:46:03 +0100

    list of products

--- products.php
+++ products.php
@@ -1,5 +1,19 @@
 <?php 
 
+$dsn = "mysql:host=localhost;dbname=leranconcepts";
+$user = "root";
+$passwd = "";
+
+$pdo = new PDO($dsn, $user, $passwd);
+
 // managing products 
+
+$sth = $dbh->prepare("SELECT * product");
+$sth->execute();
+
+/* Fetch all of the remaining rows in the result set */
+$result = $sth->fetchAll();
+print_r($result);
+
 
 ?>
